eb804569892521c67bcda3313d4a25c913daa450
Hui Peng Hu <woohp135@gmail.com>
2012-07-20 00:19:42 -0700

    slight optimization.

--- graphics/anistropic_diffusion.py
+++ graphics/anistropic_diffusion.py
@@ -1,6 +1,5 @@
 import scipy as sp
 from scipy.ndimage import convolve
-import cv2
 
 kernel_N = sp.array([[1], [-1], [0]])
 kernel_S = sp.array([[0], [-1], [1]])
@@ -10,7 +9,7 @@ kernel_W = sp.array([[1, -1, 0]])
 def g(x, kappa):
     return sp.exp(-((x / kappa) ** 2))
 
-def shift(img, dx, dy):
+def shift(img, dy, dx):
     rows, cols = img.shape
     result = sp.zeros(img.shape, dtype=img.dtype)
     result[max(dy, 0):rows+dy, max(dx, 0):cols+dx] = img[max(-dy, 0):rows-dy, max(-dx, 0):cols-dx]
@@ -19,10 +18,15 @@ def shift(img, dx, dy):
 def anistropic_diffusion(I, num_iter=15, delta=1.0/8, kappa=10):
     I_t = I.astype(sp.float64)
     for i in xrange(num_iter):
-        gradient_N = convolve(I_t, kernel_N)
-        gradient_S = convolve(I_t, kernel_S)
-        gradient_E = convolve(I_t, kernel_E)
-        gradient_W = convolve(I_t, kernel_W)
+        gradient_N = shift(I_t, 0, 1)
+        gradient_N -= I_t
+        gradient_S = shift(I_t, 0, -1)
+        gradient_S -= I_t
+        gradient_E = shift(I_t, 1, 0)
+        gradient_E -= I_t
+        gradient_W = shift(I_t, -1, 0)
+        gradient_W -= I_t
+
         c_N = g(sp.absolute(gradient_N), kappa)
         c_S = g(sp.absolute(gradient_S), kappa)
         c_E = g(sp.absolute(gradient_E), kappa)
