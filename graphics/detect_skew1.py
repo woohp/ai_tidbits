4200e4db19ed3f875f6a546856377a394fdbe544
Hui Peng Hu <woohp135@gmail.com>
2012-07-20 21:38:45 -0700

    log(n) optimization :)

--- graphics/detect_skew1.py
+++ graphics/detect_skew1.py
@@ -3,35 +3,60 @@ from scipy.ndimage.interpolation import rotate
 from scipy.misc import imresize
 import cv2
 
-def detect_skew(img, min_angle=-45, max_angle=45):
+def detect_skew(img, min_angle=-20, max_angle=20, quality='low'):
     rows, cols = img.shape
+    min_min_angle = min_angle
+    max_max_angle = max_angle
+
+    if quality == 'low':
+        resolution = sp.arctan2(2.0, cols) * 180.0 / sp.pi
+        min_target_size = 100
+        resize_order = 1
+    elif quality == 'high':
+        resolution = sp.arctan2(1.0, cols) * 180.0 / sp.pi
+        min_target_size = 300
+        resize_order = 3
+    else:
+        resolution = sp.arctan2(1.0, cols) * 180.0 / sp.pi
+        min_target_size = 200
+        resize_order = 2
 
     # resize the image so it's faster to work with
     min_size = min(rows, cols)
-    new_min_size = 100 if min_size > 100 else min_size
-    resize_ratio = float(new_min_size) / min_size
-    print resize_ratio
+    target_size = min_target_size if min_size > min_target_size else min_size
+    resize_ratio = float(target_size) / min_size
     img = imresize(img, resize_ratio)
     rows, cols = img.shape
 
-    # pad the image
-    img = sp.zeros(img.shape) + 255 - img # flip the image
+    # pad the image and invert the colors
+    img *= -1
+    img += 255
     padded_img = sp.zeros((rows*2, cols*2))
     padded_img[rows//2:rows//2+rows, cols//2:cols//2+cols] = img
     img = padded_img
 
-    # rotate the image by various degrees, see which angle gives highest var
-    best_variance = 0
-    for angle in xrange(min_angle, max_angle+1):
-        rotated_img = rotate(img, angle, reshape=False, order=1)
-        num_black_pixels = sp.sum(rotated_img, axis=1) / cols
-        variance = sp.var(num_black_pixels)
-        if variance > best_variance:
-            best_angle = angle
-            best_variance = variance
-        print angle, variance
+    # keep dividing the interval in half to achieve O(log(n))
+    while True:
+        current_resolution = (max_angle - min_angle) / 4.0
+        best_angle = None
+        best_variance = 0.0
 
-    print 'best:', best_angle
-        
+        # rotate the image, sum the pixel values in each row for each rotation
+        # then find the variance of all the sums, pick the highest variance
+        for i in xrange(5):
+            angle = min_angle + i * current_resolution
+            rotated_img = rotate(img, angle, reshape=False, order=resize_order)
+            num_black_pixels = sp.sum(rotated_img, axis=1)
+            variance = sp.var(num_black_pixels)
+            if variance > best_variance:
+                best_angle = angle
+                best_variance = variance
 
-detect_skew(cv2.imread('/media/sf_dev/scan2.png', 0))
+        if current_resolution < resolution:
+            break
+        # update the angle range
+        min_angle = max(best_angle - current_resolution, min_min_angle)
+        max_angle = min(best_angle + current_resolution, max_max_angle)
+
+    return best_angle
+
